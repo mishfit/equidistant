1f377297f0cb74822e8d0422cea2d65668d510d3
mishfit <mishochu@gmail.com>
2015-09-18 21:23:32 -0600

    add riddle

---
+++ README.markdown
@@ -0,0 +1,27 @@
+# Equidistant
+A colleague posted this riddle on our team wall and I've created a
+programmatic way to solve it:
+
+> You have the following numbers:
+> 1, 1, 2, 2, 3, 3, 4, 4
+> Sort the numbers such that all pair of numbers have a distance of
+> their [individual] value. e.g.:
+> [1, 2, 1] :+1: (the pair of ones have exactly one digit between them)
+> [1, 1, 2] :-1:
+
+## Requirements
+The example requires [node](https://nodejs.org/en/) and [npm](https://www.npmjs.com/) to run
+
+```bash
+git clone https://github.com/mishfit/equidistant.git
+cd equidistant
+npm install .
+node equidistant.js
+```
+
+Initially the script run will error out (this is intentional).
+**Modify the "sort" function until tests pass**. The answer to the
+original riddle is part of the test and is slightly *"obfuscated"*.
+Obviously this is based on the "honor" system, however,
+if you'd like to see the answer head on over to the ["answer"](https://github.com/mishfit/equidistant/tree/answer) branch.
+
---
+++ equidistant.js
@@ -0,0 +1,82 @@
+var assert = require('assert'),
+    btoa = require('btoa'),
+    atob = require('atob');
+  
+/********************************************************************************
+ * Write a function which, 
+ * 1. takes in an array of integers
+ * 2. sorts given integers, such that any member of an identical pair of numbers,
+ *    a. is seperated from its pair by the same number of digits as its value
+ *        i. e.g. given [1, 2, 1]
+ *           the difference of the indices equals the sum of the members of the pair
+ *           or, 2(third item in array) - 0(first item in array) - 1 = 1
+ * 3. can identify invalid array inputs and return -1 (not an array containing -1)
+ *    a. unpaired integers in the array
+ *    b. arrays containing odd numbers of items
+*********************************************************************************
+ * YOUR CODE BELOW
+********************************************************************************/
+function sort(array) {
+
+};
+/********************************************************************************
+ * ARRAY COMPARER - used by 'assert' to check resulting arrays
+********************************************************************************/
+function arraysEqual(x, y) {
+    // if the other y is a falsy value, return
+    if (!y)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (x.length != y.length)
+        return false;
+
+    for (var i = 0, l=x.length; i < l; i++) {
+        // Check if we have nested ys
+        if (x[i] instanceof Array && y[i] instanceof Array) {
+            // recurse into the nested ys
+            if (!x[i].equals(y[i]))
+                return false;       
+        }           
+        else if (x[i] != y[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
+/******************************************************************************
+ * SUPPLEMENTARY FUNCTIONS TO "HIDE" THE ORIGINAL RIDDLE'S ANSWER
+******************************************************************************/
+
+function obfuscateNumber(value) {
+  if (typeof value === 'number') {
+    return btoa(encodeURIComponent(escape(Number.prototype.toString.call(value, 16))));
+  } else {
+    console.log(value + ' is not a number, it is a ' + typeof value);
+  }
+}
+function parseObfuscatedNumber(value) {
+  if (typeof value === 'string') {
+    return parseInt(unescape(decodeURIComponent(atob(value))), 16);
+  } else {
+    console.log(value + ' is not a string');
+  }
+}
+/********************************************************************************
+ * TESTS
+********************************************************************************/
+// just making sure obfuscation works (obviously this is very much based
+// on the "honor" system
+assert.equal('YQ==', obfuscateNumber(10));
+assert.equal(10, parseObfuscatedNumber('YQ=='));
+
+// real tests
+assert.equal(-1, sort([1, 1, 2, 2, 3, 3, 4]));
+
+// hard to assert your function is correct without giving away
+// the answer to Darren's riddle
+assert.equal(['NA==', 'MQ==', 'Mw==', 'MQ==', 'Mg==', 'NA==', 'Mw==', 'Mg=='],
+  sort([1, 1, 2, 2, 3, 3, 4, 4]));
+
+console.log('succeeded!');
